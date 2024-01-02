8281d4ca1f4bc5302a90b30f14dc36452a2d360e
Anish <aniish9.18@gmail.com>
2024-01-02 19:19:42 +0530

    Django

--- DjangoApp/core/views.py
+++ DjangoApp/core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import MeetFile
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'index.html', {})
+
+def uploadpage(request):
+    if request.method == 'POST':
+        file = request.FILES['file']
+        mf = MeetFile(file=file)
+        mf.save()
+    return render(request, 'uploadpage.html', {})
+
+
