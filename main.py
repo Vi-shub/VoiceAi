4a0c66ad05c7867a9e9890e2d3221134d968d512
Anish <aniish9.18@gmail.com>
2024-01-02 17:29:30 +0530

    c

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def main():
         os.makedirs("Meet_Files")
     with open("input.txt","w") as f:
         f.write("0")
-    # pre_meeting()
+    pre_meeting()
     while_meeting()
     with open("Transcript.txt","w") as out:
         out.write("")
