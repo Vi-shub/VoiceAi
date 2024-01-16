a2ec812b5405a7e150ba35632dee1ca5a75e7564
Bodhisatya Ghosh <rommelghosh@gmail.com>
2024-01-16 15:36:27 +0530

    fixed analysis

--- audio sentiment/pred.py
+++ audio sentiment/pred.py
@@ -6,44 +6,41 @@ import numpy as np
 import librosa
 import os
 
-
 def ser(path):
     model = load_model("./ser_best_5868.h5")
     emotions = ["neutral", "calm", "happy", "sad", "angry", "fearful", "disgust", "surprised"]
+    prediction_list = []
 
-    def get_features(path):
+    def get_features(audio, s_r):
         mfcc = []
         zcr = []
         rmse = []
 
+        # Extract features
+        mel_freq = np.mean(librosa.feature.mfcc(y=audio, sr=s_r, n_mfcc=50).T, axis=0).reshape(-1)
+        zero_cross = np.pad(librosa.feature.zero_crossing_rate(y=audio).reshape(-1), (0, 228 - len(librosa.feature.zero_crossing_rate(y=audio).reshape(-1))), 'constant')
+        rms = np.pad(librosa.feature.rms(y=audio).reshape(-1), (0, 228 - len(librosa.feature.rms(y=audio).reshape(-1))), 'constant')
 
-        all_features = []
-        audio,s_r = librosa.load(path)
-
-        mel_freq = np.mean(librosa.feature.mfcc(y = audio, sr = s_r, n_mfcc=50).T, axis = 0).reshape(-1)
-
-        zero_cross = np.pad((librosa.feature.zero_crossing_rate(y = audio).reshape(-1)),
-                                (0,(228-len(librosa.feature.zero_crossing_rate(y = audio).reshape(-1)))),
-                                'constant',
-                                constant_values=(0,0))
-
-        rms = np.pad((librosa.feature.rms(y = audio).reshape(-1)),
-                                (0,(228-len(librosa.feature.rms(y = audio).reshape(-1)))),
-                                'constant',
-                                constant_values=(0,0))
-        
-        
         mfcc.append(mel_freq)
         zcr.append(zero_cross)
         rmse.append(rms)
 
-        all_features.append(np.hstack((mel_freq,zero_cross,rms)))
+        all_features = np.hstack((mel_freq, zero_cross, rms)).reshape(1, -1)
 
         return all_features
 
-    # path = input("Enter file path: ")
-    x = np.array(get_features(path)).reshape(1,-1)
+    audio, s_r = librosa.load(path)
 
-    pred = emotions[np.argmax(model.predict(x))]
-    return pred
+    # chunk duration 2 seconds
+    chunk_duration = 3  
+    chunk_samples = int(chunk_duration * s_r)
+    chunks = [audio[i:i + chunk_samples] for i in range(0, len(audio), chunk_samples)]
 
+    for i, chunk in enumerate(chunks):
+        x = np.array(get_features(chunk, s_r))
+        pred = emotions[np.argmax(model.predict(x))]
+        prediction_list.append(pred)
+
+    return prediction_list
+
+print(ser(r"C:\Users\Rommel\OneDrive\Documents\Sound Recordings\Recording (4).wav"))
